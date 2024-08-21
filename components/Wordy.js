bf7a36bc64a20129d4e2e18ee7c6f8ca95b29790
Guillermo <guillermo.dv@egmail.com>
2024-08-21 18:06:03 -0300

    fix keyboard

--- components/Wordy.js
+++ components/Wordy.js
@@ -1,8 +1,38 @@
 import useWordy from "@/hooks/useWordy";
 import { useEffect, useState } from "react";
 import Grid from "./Grid";
-import Keypad from "./Keypad";
 import Modal from "./Modal";
+
+import { FaBackspace, FaCheck } from "react-icons/fa";
+
+const letters = [
+  { key: "a" },
+  { key: "b" },
+  { key: "c" },
+  { key: "d" },
+  { key: "e" },
+  { key: "f" },
+  { key: "g" },
+  { key: "h" },
+  { key: "i" },
+  { key: "j" },
+  { key: "k" },
+  { key: "l" },
+  { key: "m" },
+  { key: "n" },
+  { key: "o" },
+  { key: "p" },
+  { key: "q" },
+  { key: "r" },
+  { key: "s" },
+  { key: "t" },
+  { key: "u" },
+  { key: "v" },
+  { key: "w" },
+  { key: "x" },
+  { key: "y" },
+  { key: "z" },
+];
 
 export default function Wordy({ solution }) {
   const { currentGuess, handleKeyUp, guesses, isCorrect, turn, usedKeys } =
@@ -27,18 +57,55 @@ export default function Wordy({ solution }) {
     return () => window.removeEventListener("keyup", handleKeyUp);
   }, [handleKeyUp]);
 
-  useEffect(() => {
-    console.log(guesses, turn, isCorrect);
-  }, [guesses, turn, isCorrect]);
+  const handleClick = (key) => {
+    handleKeyUp({ key });
+  };
 
   return (
     <>
-      <h1>Solution: {solution}</h1>
+      {console.log("solution", solution)}
       <Grid currentGuess={currentGuess} guesses={guesses} turn={turn} />
-      <Keypad usedKeys={usedKeys} />
-      {showModal && (
-        <Modal isCorrect={isCorrect} turn={turn} solution={solution} />
-      )}
+      <div className="keypad">
+        {letters &&
+          letters.map((letter) => {
+            const color = usedKeys[letter?.key];
+            return (
+              <div
+                className={color}
+                key={letter.key}
+                onClick={() => {
+                  handleClick(letter.key);
+                }}
+              >
+                {letter.key}
+              </div>
+            );
+          })}
+      </div>
+      <div className="keypadFooter">
+        <button
+          className="action-button"
+          key={"enter"}
+          onClick={() => {
+            handleClick("Enter");
+          }}
+        >
+          <FaCheck /> {/* Ícono de Enter */}
+        </button>
+        <button
+          className="action-button"
+          key={"backspace"}
+          onClick={() => {
+            handleClick("Backspace");
+          }}
+        >
+          <FaBackspace />
+        </button>
+
+        {showModal && (
+          <Modal isCorrect={isCorrect} turn={turn} solution={solution} />
+        )}
+      </div>
     </>
   );
 }
