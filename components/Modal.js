ba012be7fd19a5bd8c2d7a2113eb38f93d043d99
guillermo.dv <guillermo.dv@gmail.com>
2024-09-09 12:57:32 -0300

    fix(modal): fix styles

--- components/Modal.js
+++ components/Modal.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { FaShareAlt, FaTimes } from "react-icons/fa";
 
 const handleReset = () => {
@@ -11,7 +12,22 @@ const handleCopyToClipboard = (turn, solution) => {
   });
 };
 
+const fetchMeme = async (word) => {
+  //const response = await fetch(`https://meme-api.com/getMeme?word=${word}`);
+  //const data = await response.json();
+  //return data.url;
+  return "https://www.latercera.com/resizer/SiNfPY7FHBb1-8UM0hCLXGO8wCI=/800x0/smart/cloudfront-us-east-1.images.arcpublishing.com/copesa/7FFVS6CFZNBA7PZCTJILH7MJSA.jpeg"
+};
+
 export default function Modal({ isCorrect, turn, solution, titles }) {
+  const [memeUrl, setMemeUrl] = useState(null);
+
+  useEffect(() => {
+    if (solution) {
+      fetchMeme(solution).then((url) => setMemeUrl(url));
+    }
+  }, [solution]);
+
   return (
     <div className="modal">
       <div className="modal-content">
@@ -20,15 +36,20 @@ export default function Modal({ isCorrect, turn, solution, titles }) {
         </button>
         {isCorrect ? (
           <>
-            <h1 className="solution">{titles.win}</h1>
+            <h1 className="solution">{titles.win}  </h1>
             <p className="gamification">{titles.winDescription}</p>
-
+            {solution && (
+          <>
+            <p className="memelabel">The meme of the day related to {solution} is:</p>
+            <img src={memeUrl} alt={`Meme related to ${solution}`} className="meme" />
             <button
               onClick={() => handleCopyToClipboard(turn, solution)}
               className="share-button"
             >
               <FaShareAlt /> Share your result
             </button>
+          </>
+        )}
           </>
         ) : (
           <>
@@ -37,15 +58,16 @@ export default function Modal({ isCorrect, turn, solution, titles }) {
             <p className="gamification">{titles.looseDescription}</p>
           </>
         )}
+        
 
-          <p className="gamification">
-            Want to save your progress?{" "}
-            <a href="/login" className="login-link">
-              Log in here
-            </a>
-            .
-          </p>
-        </div>
+        <p className="gamification">
+          Want to save your progress?{" "}
+          <a href="/login" className="login-link">
+            Log in here
+          </a>
+          .
+        </p>
+      </div>
     </div>
   );
 }
